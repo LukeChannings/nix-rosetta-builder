549cbfbdd4ae6c36aef6f0d865e3d3b93c250941
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2025-01-28 20:51:04 +0000

    Use Lix

--- flake.nix
+++ flake.nix
@@ -2,24 +2,28 @@
   description = "Lima-based, Rosetta 2-enabled, Apple silicon (macOS/Darwin)-hosted Linux builder";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs";
+
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # See https://lix.systems/add-to-config/#flake-based-configurations for the latest version
+    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+    lix-module.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixos-generators, nixpkgs }:
+  outputs = { self, nixos-generators, nixpkgs, lix-module }:
   let
     darwinSystem = "aarch64-darwin";
     linuxSystem = builtins.replaceStrings [ "darwin" ] [ "linux" ] darwinSystem;
-    lib = nixpkgs.lib;
 
   in {
     packages."${linuxSystem}" =
     let
-      arguments = { inherit linuxSystem nixos-generators nixpkgs; };
-      pkgs = nixpkgs.legacyPackages."${linuxSystem}";
+      arguments = { inherit linuxSystem nixos-generators nixpkgs lix-module; };
+      pkgs = import nixpkgs { system = linuxSystem; overlays = [ lix-module.overlays.default ]; };
     in {
       default = pkgs.callPackage ./package.nix (arguments // { onDemand = false; });
       on-demand = pkgs.callPackage ./package.nix (arguments // { onDemand = true; });
